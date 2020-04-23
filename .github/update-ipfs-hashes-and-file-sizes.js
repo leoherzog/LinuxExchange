d58f35474539a0930e50abdb3a046ef4bb1b888a
Leo <5376265+leoherzog@users.noreply.github.com>
2020-04-23 12:59:35 -0400

    Break Out Adding File Size into Separate Function

--- .github/update-ipfs-hashes-and-file-sizes.js
+++ .github/update-ipfs-hashes-and-file-sizes.js
@@ -7,7 +7,7 @@ var node;
 var distros = JSON.parse(fs.readFileSync('distros.json'));
 var timeInBase64 = new Buffer(new Date().getTime().toString()).toString('base64');
 
-async function startNode () {
+async function startNode() {
   node = await ipfs.create();
   await downloadFiles(node);
   process.exit();
@@ -18,14 +18,7 @@ async function downloadFiles() {
     for (let version of distro.versions) {
       var url = version['direct-download-url'].replace('{{base64time}}', timeInBase64);
       if (!version['file-size']) {
-        try {
-          var res = await fetch(url, {"timeout": 60 * 1000, "headers": {"user-agent": "Wget/"}});
-          version['file-size'] = res.headers.get('content-length');
-          fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
-        }
-        catch(e) {
-          console.log("Trouble downloading " + url.substring(url.lastIndexOf('/') + 1) + ": " + e.toString());
-        }
+        await addFileSize(version, url);
       }
       if (!version['ipfs-hash']) {
         await addHash(version, url);
@@ -35,19 +28,29 @@ async function downloadFiles() {
   return;
 }
 
+async function addFileSize(version, url) {
+  try {
+    var res = await fetch(url, {"timeout": 60 * 1000, "headers": {"user-agent": "Wget/"}});
+    version['file-size'] = res.headers.get('content-length');
+    fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
+  }
+  catch(e) {
+    console.log("Problem adding file size on " + url.substring(url.lastIndexOf('/') + 1) + ": " + e.toString());
+  }
+}
+
 async function addHash(version, url) {
   try {
     for await (const file of node.add(urlSource(url, {"timeout": 60 * 1000, "headers": {"user-agent": "Wget/"}}), {"pin": false})) {
       version['ipfs-hash'] = file.cid.toString();
       fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
       console.log('Added hash for ' + url.substring(url.lastIndexOf('/') + 1));
-      await node.pin.rm(file.cid.toString());
       await node.repo.gc();
       return;
     }
   }
   catch(e) {
-    console.error('Problem downloading: ' + e.toString());
+    console.error("Problem adding hash on " + url.substring(url.lastIndexOf('/') + 1) + ": " + e.toString());
   }
 }
 
