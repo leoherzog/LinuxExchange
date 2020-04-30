e676b2a9cc44b9d22f39fad49850f4fae762b06a
Leo Herzog <xd1936@gmail.com>
2020-04-29 22:36:19 -0400

    v0.1

---
+++ README.md
@@ -0,0 +1,53 @@
+# Linux Exchange
+
+### What is this?
+
+Creating and maintaining a distribution of Linux is hard enough without operating a network of mirrors.
+
+What if we could combine the convenience of downloading large files via http in the web browser, with the scalability and efficiency of torrenting? This website serves as a demo for downloading Linux ISOs via [IPFS](https://ipfs.io/#how), entirely on a static site.
+
+That means that the more people that use this website, and the more distros that integrate IPFS for distribution of their installation media, the faster it gets for everyone! If you maintain a version of Linux, it's easier than ever to distribute your installation media peer-to-peer, no mirror network required.
+
+### I want to help!
+
+Awesome! Consider becoming an IPFS node that hosts this content. You can either use [the Linux Exchange website](https://linux.exchange/) itself, or run a [Linux Exchange IPFS Seedbox](https://github.com/leoherzog/LinuxExchangeIPFSSeedbox).
+
+If you have thoughts about enhancing the code, design, or backend automation (currently trying out [Github Actions](https://github.com/leoherzog/LinuxExchange/tree/master/.github)) of this website, I'd be happy to review Pull Requests. The magic of [`js-ipfs`](https://js.ipfs.io/) is, you can fork this code and publish your own version... They'll still talk to each other, regardless of domain!
+
+### License
+
+Feel free to take a look at the source and adapt as you please. This source is licensed as follows:
+
+[![Creative Commons License](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-sa/4.0/)
+
+Linux Exchange is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/).
+
+- - - -
+
+#### About Me
+
+<a href="https://herzog.tech/" target="_blank">
+  <img src="https://herzog.tech/signature/link.svg.png" width="32px" />
+</a>
+<a href="https://twitter.com/downtime" target="_blank">
+  <img src="https://herzog.tech/signature/twitter.svg.png" width="32px" />
+</a>
+<a href="https://facebook.com/xd1936" target="_blank">
+  <img src="https://herzog.tech/signature/facebook.svg.png" width="32px" />
+</a>
+<a href="https://github.com/leoherzog" target="_blank">
+  <img src="https://herzog.tech/signature/github.svg.png" width="32px" />
+</a>
+<a href="https://keybase.io/leoherzog" target="_blank">
+  <img src="https://herzog.tech/signature/keybase.svg.png" width="32px" />
+</a>
+<a href="https://www.linkedin.com/in/leoherzog" target="_blank">
+  <img src="https://herzog.tech/signature/linkedin.svg.png" width="32px" />
+</a>
+<a href="https://hope.edu/directory/people/herzog-leo/" target="_blank">
+  <img src="https://herzog.tech/signature/anchor.svg.png" width="32px" />
+</a>
+<br />
+<a href="https://www.buymeacoffee.com/leoherzog" target="_blank">
+  <img src="https://cdn.buymeacoffee.com/buttons/lato-black.png" alt="Buy Me A Coffee" width="217px" />
+</a>
---
+++ lx.js
@@ -0,0 +1,258 @@
+var node;
+var distros;
+var selectedDistro;
+var selectedVersion;
+var os = document.getElementById('os');
+var version = document.getElementById('version');
+var start = document.getElementById('start');
+var versionNumber = document.getElementById('version-number');
+var desktopEnvironment = document.getElementById('desktop-environment');
+var arch = document.getElementById('arch');
+var nodeStatusIcon = document.getElementById('nodeStatusIcon');
+var nodeStatusText = document.getElementById('nodeStatusText');
+var downloads = document.getElementById('downloads');
+
+window.onload = load;
+
+async function load() {
+
+  updateNodeStatus('loading', 'Fetching info about Distros...');
+
+  try {
+    let res = await fetch('distros.json');
+    distros = await res.json();
+  }
+  catch(e) {
+    updateNodeStatus('error', 'Problem loading Distros information. ' + e.toString());
+    console.error(e.toString());
+    setTimeout(load, 10000);
+    return;
+  }
+
+  os.innerHTML = '';
+  for (let distro of distros.distros) {
+    let option = document.createElement("option");
+    option.text = distro['name'];
+    os.add(option);
+  }
+  os.selectedIndex = distros['recommended-distro-index'];
+
+  onDistroChange();
+  os.addEventListener('change', onDistroChange);
+  onVersionChange();
+  version.addEventListener('change', onVersionChange);
+  start.addEventListener('click', download);
+
+  try {
+    updateNodeStatus('loading', 'Turning this browser tab into an IPFS node...');
+    await startNode();
+    setInterval(() => node.swarm.peers().then(peers => console.log("Peers: " + peers.length)), 15000);
+  }
+  catch(e) {
+    updateNodeStatus('error', 'Problem starting node. ' + e.toString());
+    setTimeout(load, 15000);
+  }
+
+}
+
+function onDistroChange() {
+
+  selectedDistro = distros.distros.find(index => index['name'] == os.value);
+
+  // boolean on whether this distro has only one unique attribute
+  let hasSameVersion = selectedDistro.versions.every((element, index, array) => element['version'] == array[0]['version']);
+  let hasSameDE = selectedDistro.versions.every((element, index, array) => element['desktop-environment'] == array[0]['desktop-environment']);
+  let hasSameArch = selectedDistro.versions.every((element, index, array) => element['arch'] == array[0]['arch']);
+
+  version.innerHTML = '';
+  for (let version of selectedDistro.versions) {
+    let properties = [];
+    if (!hasSameVersion) properties.push(version['version']);
+    if (!hasSameDE) properties.push(version['desktop-environment']);
+    if (!hasSameArch) properties.push(version['arch']);
+    let option = document.createElement('option');
+    option.text = properties.join(' — ') || version['version'];
+    if (version['ipfs-hash']) {
+      option.value = version['ipfs-hash'];
+    } else {
+      option.disabled = true;
+      option.text += ' (temporarily unavailable)'
+    }
+    document.getElementById('version').add(option);
+  }
+
+  version.disabled = selectedDistro.versions.length == 1;
+
+  version.selectedIndex = selectedDistro['recommended-version-index'];
+
+  onVersionChange();
+
+}
+
+function onVersionChange(element) {
+
+  selectedDistro = distros.distros.find(index => index['name'] == os.value);
+  selectedVersion = selectedDistro.versions.find(index => index['ipfs-hash'] == version.value);
+
+  let selectedDesktopEnvironment = distros['desktop-environments'][selectedVersion['desktop-environment']];
+  
+  versionNumber.innerHTML = selectedDistro['full-name'] + " " + selectedVersion['version'];
+  if (selectedDistro['font-logos-icon']) {
+    versionNumber.innerHTML = '<span class="' + selectedDistro['font-logos-icon'] +'"></span> ' + versionNumber.innerHTML;
+  }
+  versionNumber.setAttribute('title', selectedDistro['description']);
+  if (selectedDistro['url']) {
+    versionNumber.setAttribute('onclick', 'window.open("' + selectedDistro['url'] + '", "_blank")');
+  } else {
+    versionNumber.setAttribute('onclick', '');
+  }
+
+  desktopEnvironment.innerHTML = selectedVersion['desktop-environment'];
+  desktopEnvironment.setAttribute('title', selectedDesktopEnvironment['description']);
+  if (selectedDesktopEnvironment['url']) {
+    desktopEnvironment.setAttribute('onclick', 'window.open("' + selectedDesktopEnvironment['url'] + '", "_blank")');
+  } else {
+    desktopEnvironment.setAttribute('onclick', '');
+  }
+
+  arch.innerHTML = selectedVersion['arch'];
+  arch.setAttribute('title', distros['architectures'][selectedVersion['arch']]);
+
+}
+
+async function startNode(event) {
+  console.log("Starting IPFS...");
+  node = await Ipfs.create();
+  let ipfsVersion = await node.version();
+  console.log("Started, version " + ipfsVersion.version);
+  updateNodeStatus('ready', 'This browser tab is a v' + ipfsVersion.version + ' node connected to the IPFS network');
+}
+
+async function download() {
+
+  let hash = selectedVersion['ipfs-hash'];
+  let name = selectedVersion['direct-download-url'].substring(selectedVersion['direct-download-url'].lastIndexOf('/') + 1);
+  let total = selectedVersion['file-size'];
+
+  let row = document.getElementById(hash);
+  if (!row) {
+    console.log("Starting download for " + name);
+    row = createRow(hash);
+    downloads.appendChild(row);
+  } else {
+    console.error("Already downloading " + name);
+  }
+
+  let progressStatus = document.getElementById(hash + '-progress');
+
+  for await (const file of node.get(hash)) {
+
+    var content = new Blob([], {"type": "application/octet-stream"});
+    let progress = 0;
+    for await (const chunk of file.content) {
+      if (!progress) document.getElementById(hash + '-total').innerHTML = ' / ' + filesize(total) + ' <span class="fad fa-spinner fa-fw fa-pulse"></span>';
+      progress += chunk.byteLength;
+      content = new Blob([content, chunk], {"type": "application/octet-stream"});
+      if (progress < 1073741824) {
+        progressStatus.innerHTML = filesize(progress, {"round": 0});
+      } else {
+        progressStatus.innerHTML = filesize(progress, {"round": 1});
+      }
+    }
+
+    console.log("Saving " + name);
+    
+    var a = document.createElement('a');
+    var url = window.URL.createObjectURL(content);
+    a.setAttribute("href", url);
+    a.setAttribute("download", name);
+    a.innerHTML = 'Save Again';
+    document.getElementById(hash + '-progress').innerHTML = '';
+    document.getElementById(hash + '-progress').appendChild(a);
+    document.getElementById(hash + '-total').innerHTML = '(' + filesize(total) + ') <span class="far fa-file-check fa-fw ready"></span>';
+    a.click();
+    console.log("Saved " + name);
+
+  };
+
+}
+
+function createRow(hash) {
+
+  row = document.createElement('div');
+  row.setAttribute('id', hash);
+  row.className = 'row';
+
+  let left = document.createElement('div');
+  let right = document.createElement('div');
+
+  let col1 = document.createElement('div');
+  col1.setAttribute('id', hash + '-distroicon');
+  col1.className = 'cell';
+  col1.innerHTML = '<span class="' + (selectedDistro['font-logos-icon'] || 'fab fa-linux') +'"></span>';
+  left.appendChild(col1);
+  
+  let col2 = document.createElement('div');
+  col2.setAttribute('id', hash + '-name');
+  col2.className = 'cell';
+  col2.innerHTML = selectedDistro['full-name'] + ' ' + selectedVersion['version'] + ' (' + selectedVersion['desktop-environment'] + ', ' + selectedVersion['arch'] + ')';
+  left.appendChild(col2);
+  
+  let col3 = document.createElement('div');
+  col3.setAttribute('id', hash + '-progress');
+  col3.className = 'cell';
+  col3.innerHTML = 'Requesting...';
+  right.appendChild(col3);
+  
+  let col4 = document.createElement('div');
+  col4.setAttribute('id', hash + '-total');
+  col4.className = 'cell';
+  col4.innerHTML = filesize(selectedVersion['file-size']) + ' <span class="fad fa-users fa-fw blink"></span>';
+  right.appendChild(col4);
+
+  row.appendChild(left);
+  row.appendChild(right);
+  
+  return row;
+
+}
+
+function updateNodeStatus(status, message) {
+  nodeStatusIcon.className = status;
+  nodeStatusText.className = status;
+  switch (status) {
+    case "loading":
+      nodeStatusIcon.innerHTML = '<span class="fa-stack"><span class="fas fa-circle fa-stack-2x"></span><span class="fas fa-spinner fa-pulse fa-stack-1x fa-inverse"></span></span>';
+      nodeStatusIcon.setAttribute('title', message);
+      nodeStatusText.innerText = 'about to be connected directly to the IPFS network';
+      start.disabled = true;
+      break;
+    case "ready":
+      nodeStatusIcon.innerHTML = '<span class="fa-stack"><span class="fas fa-circle fa-stack-2x"></span><span class="far fa-chart-network fa-stack-1x fa-inverse"></span></span>';
+      nodeStatusIcon.setAttribute('title', message);
+      nodeStatusText.innerText = 'currently connected directly to the IPFS network';
+      start.disabled = false;
+      break;
+    case "error":
+      nodeStatusIcon.innerHTML = '<span class="fa-stack"><span class="fas fa-circle fa-stack-2x"></span><span class="fas fa-exclamation fa-stack-1x fa-inverse"></span></span>';
+      nodeStatusIcon.setAttribute('title', message);
+      nodeStatusText.innerText = 'capable of being connected to the IPFS network, but we ran into a snag (' + message + ')';
+      start.disabled = true;
+      break;
+  }
+}
+
+Array.prototype.unique = function() { 
+  var seen = {};
+  var out = [];
+  var len = this.length;
+  var j = 0;
+  for (var i = 0; i < len; i++) {
+    var item = this[i];
+    if (seen[item] !== 1) {
+      seen[item] = 1;
+      out[j++] = item;
+    }
+  }
+  return out;
+}
