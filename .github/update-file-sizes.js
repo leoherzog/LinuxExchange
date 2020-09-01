903506edb23def867a320d9246229dc903203cc8
Leo <5376265+leoherzog@users.noreply.github.com>
2020-09-01 10:32:45 -0400

    Remove Extra IPFS Code

--- .github/update-file-sizes.js
+++ .github/update-file-sizes.js
@@ -28,19 +28,4 @@ async function addFileSize(version, url) {
   }
 }
 
-async function addHash(version, url) {
-  try {
-    for await (const file of node.add(urlSource(url, {"timeout": 60 * 1000, "headers": {"user-agent": "Wget/"}}), {"pin": false})) {
-      version['ipfs-hash'] = file.cid.toString();
-      fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
-      console.log('Added hash for ' + url.substring(url.lastIndexOf('/') + 1));
-      await node.repo.gc();
-      return;
-    }
-  }
-  catch(e) {
-    console.error("Problem adding hash on " + url.substring(url.lastIndexOf('/') + 1) + ": " + e.toString());
-  }
-}
-
 downloadFiles();
