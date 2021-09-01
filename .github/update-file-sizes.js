cd2ae09b97171640678815cca1b9f62dff863dda
Leo <5376265+leoherzog@users.noreply.github.com>
2021-09-01 19:05:36 +0000

    Switch from node-fetch to built-in http/https libs

--- .github/update-file-sizes.js
+++ .github/update-file-sizes.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
-import fetch from 'node-fetch';
+const http = require('http');
+const https = require('https');
 
 var node;
 var distros = JSON.parse(fs.readFileSync('distros.json'));
-var timeInBase64 = new Buffer(new Date().getTime().toString()).toString('base64');
+var timeInBase64 = new Buffer.from(new Date().getTime().toString()).toString('base64');
 
-async function downloadFiles() {
+function downloadFiles() {
   for (let distro of distros.distros) {
     for (let version of distro.versions) {
       var url = version['direct-download-url'].replace('{{base64time}}', timeInBase64);
       if (!version['file-size']) {
-        await addFileSize(version, url);
+        addFileSize(version, url);
       }
     }
   }
   return;
 }
 
-async function addFileSize(version, url) {
-  try {
-    var res = await fetch(url, {"timeout": 60 * 1000, "headers": {"user-agent": "Wget/"}});
-    version['file-size'] = await res.headers.get('content-length');
-    fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
-  }
-  catch(e) {
-    console.log("Problem adding file size on " + url.substring(url.lastIndexOf('/') + 1) + ": " + e.toString());
+function addFileSize(version, url) {
+  if (url.toLowerCase().includes('https')) {
+    https.get(url, function(res) {
+      version['file-size'] = res.headers['content-length'];
+      fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
+    });
+  } else {
+    http.get(url, function(res) {
+      version['file-size'] = res.headers['content-length'];
+      fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
+    });
   }
 }
 
