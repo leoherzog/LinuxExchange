5e9b17744c1db446ad05420dbd11abe75f5ce67c
Leo <5376265+leoherzog@users.noreply.github.com>
2023-01-23 22:22:09 +0000

    Update File Size Updater to Use Native Fetch API

--- .github/update-file-sizes.js
+++ .github/update-file-sizes.js
@@ -1,38 +1,23 @@
-const fs = require('fs');
-const { http, https } = require('follow-redirects');
-const options = {
-  "headers": {
-      "User-Agent": "Wget/",
-  }
-};
+import fs from 'fs';
 
 var distros = JSON.parse(fs.readFileSync('distros.json'));
 var timeInBase64 = new Buffer.from(new Date().getTime().toString()).toString('base64');
 
-function downloadFiles() {
-  for (let distro of distros.distros) {
-    for (let version of distro.versions) {
-      var url = version['direct-download-url'].replace('{{base64time}}', timeInBase64);
-      if (!version['file-size'] || version['file-size'] === '0') {
-        addFileSize(version, url);
-      }
+var headers = new Headers({
+  "User-Agent": "Wget/"
+});
+
+for (let distro of distros.distros) {
+  for (let version of distro.versions) {
+    var url = version['direct-download-url'].replace('{{base64time}}', timeInBase64);
+    if (!version['file-size'] || version['file-size'] === '0') {
+      addFileSize(version, url);
     }
   }
-  return;
 }
 
-function addFileSize(version, url) {
-  if (url.toLowerCase().includes('https')) {
-    https.get(url, options, function(res) {
-      version['file-size'] = res.headers['content-length'] || "";
-      fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
-    });
-  } else {
-    http.get(url, options, function(res) {
-      version['file-size'] = res.headers['content-length'] || "";
-      fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
-    });
-  }
+async function addFileSize(version, url) {
+  let filesize = await fetch(url, headers).then(r => r.headers.get('content-length'));
+  version['file-size'] = filesize ? new Number(filesize) : "";
+  fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
 }
-
-downloadFiles();
