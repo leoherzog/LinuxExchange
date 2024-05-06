35c4939de3b0895bd0cd77be38f518a7ed76e88b
Leo Herzog <leo@herzog.tech>
2024-05-06 17:04:51 -0400

    Add Recommended Version Updating to Ubuntu Updater

--- .github/ubuntu-updater.js
+++ .github/ubuntu-updater.js
@@ -2,6 +2,10 @@ import fs from 'fs';
 import * as cheerio from 'cheerio';
 import * as parseTorrent from 'parse-torrent';
 
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth() + 1;
+
 var distros = JSON.parse(fs.readFileSync('distros.json'));
 
 const distroIndex = distros['distros'].findIndex((distro) => distro['name'] == 'Ubuntu');
@@ -17,11 +21,11 @@ var urls = [
 
 var desktopEnvironments = {
   'ubuntu-budgie': 'Budgie',
-  ubuntu: 'Gnome',
   kubuntu: 'KDE',
   lubuntu: 'LXQt',
   'ubuntu-mate': 'Mate',
   xubuntu: 'Xfce',
+  ubuntu: 'Gnome',
 };
 
 async function run() {
@@ -81,7 +85,7 @@ async function run() {
       .split(version + '-')[1]
       .split('-');
     let arch = afterVersion[1].replace('.iso', '');
-    let de = desktopEnvironments[name.split('-')[0]];
+    let de = desktopEnvironments[Object.keys(desktopEnvironments).find((key) => name.includes(key))];
     let magnetUrl = 'magnet:?xt=urn:btih:' + parsedTorrent['infoHash'] + '&dn=' + name;
     let directDownloadUrl = new URL(name.replace('.torrent', ''), sourceUrl).href;
     let fileSize = parsedTorrent['length'];
@@ -92,34 +96,30 @@ async function run() {
 
   distros['distros'][distroIndex]['versions'] = distroVersions;
 
+  let ltsVersion = calculateLtsVersion();
+  console.log(ltsVersion);
+  const ltsIndex = distroVersions.findIndex((v) => v.version === ltsVersion && v.arch === 'amd64' && v['desktop-environment'] === 'Gnome');
+  distros['distros'][distroIndex]['recommended-version-index'] = ltsIndex;
+
   fs.writeFileSync('distros.json', JSON.stringify(distros, null, 2));
 }
 
 function calculateCurrentVersions() {
-  const currentDate = new Date();
-  // currentDate.setMonth(currentDate.getMonth() - 3);
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1;
-
-  const ltsYear = (currentMonth < 4 ? currentYear - 2 : currentYear - (currentYear % 2)) % 100; // two digits
-  const ltsVersion = ltsYear + '.04';
-
-  let nonLtsYear = currentYear;
-  let nonLtsMonth;
-  if (currentMonth < 4) {
-    nonLtsMonth = 10; // Last non-LTS was in the previous year, October
-    nonLtsYear -= 1;
-  } else if (currentMonth < 10) {
-    nonLtsMonth = 4; // Most recent non-LTS was this year, April
-  } else {
-    nonLtsMonth = 10; // Most recent non-LTS is this year, October
-  }
-  nonLtsYear = nonLtsYear % 100; // two digits
-  const nonLtsVersion = nonLtsYear + '.' + nonLtsMonth.toString().padStart(2, '0');
-
-  let versions = [...new Set([ltsVersion, nonLtsVersion])];
-  // console.log(versions);
+  let versions = [calculateLtsVersion(), calculateNonLtsVersion()];
+  versions = [...new Set(versions)];
   return versions;
+}
+
+function calculateLtsVersion() {
+  const ltsYear = (currentMonth < 4 ? currentYear - 2 : currentYear - (currentYear % 2)) % 100; // two digits
+  return ltsYear + '.04';
+}
+
+function calculateNonLtsVersion() {
+  let nonLtsYear = currentYear;
+  let nonLtsMonth = currentMonth < 4 ? 10 : currentMonth < 10 ? 4 : 10;
+  nonLtsYear = (nonLtsYear - (nonLtsMonth === 10 ? 1 : 0)) % 100; // Adjust year for October releases
+  return nonLtsYear + '.' + nonLtsMonth.toString().padStart(2, '0');
 }
 
 run();
